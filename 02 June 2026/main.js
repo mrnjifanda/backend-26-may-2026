b7097dd7473fcc3e1781c657ab701afaf85267a6
Mr NJIFANDA <mrnjifanda@gmail.com>
2026-06-09 19:30:38 +0100

    Course for 06 June

--- 02 June 2026/main.js
+++ 02 June 2026/main.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const PORT = 4010;
 
+
+// title, content, price and quantity
+
 const userSchema = new mongoose.Schema({
   username: String,
   email: String,
@@ -12,6 +15,138 @@ const User = mongoose.model('user', userSchema);
 
 const run = () => {
   const app = express();
+  app.use(express.json());
+
+  // List all user
+  app.get('/users', async (req, res) => {
+    try {
+      const users = await User.find();
+      return res.json({
+        error: false,
+        message: "All users",
+        data: users
+      });
+    } catch (e) {
+      return res.status(500).json({
+        error: true,
+        message: "Internal server error, please try later."
+      });
+    }
+  });
+
+  // Get one user by ID
+  app.get("/users/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const user = await User.findById(id);
+      if (!user) {
+        return res.status(404).json({
+          error: true,
+          message: "User with id: " + id + " not found"
+        });
+      }
+
+      return res.json({
+        error: false,
+        message: "User found",
+        data: user
+      });
+    } catch (error) {
+      return res.status(500).json({
+        error: true,
+        message: "Internal server error, please try later"
+      });
+    }
+  });
+
+  // Create user
+  app.post("/users/create", async (req, res) => {
+    try {
+      const { username, email, password } = req.body;
+      if (!username || !email || !password) {
+        return res.status(400).json({
+          error: true,
+          message: "Error, please fill all data in form"
+        });
+      }
+
+      await User.create({
+        username,
+        email,
+        password
+      });
+
+      return res.status(201).json({
+        error: false,
+        message: "User created successfuly"
+      });
+    } catch (error) {
+      return res.status(500).json({
+        error: true,
+        message: "Internal server error, please try later"
+      });
+    }
+  });
+
+  // Update one user with ID
+  app.put('/users/update/:id', async (req, res) => {
+    try {
+      const { id } = req.params;
+      const data = req.body;
+
+      // 1 st
+      // const user = await User.findById(id);
+      // if (!user) {
+      //   return res.status(404).json({
+      //     error: true,
+      //     message: "User not found, please verify id"
+      //   });
+      // }
+
+      // user.username = data.username;
+      // user.email = data.email;
+      // user.password = data.password;
+
+      // await user.save();
+
+      // 2nd
+      const update = await User.findByIdAndUpdate(id, data);
+      if (!update) {
+        return res.status(404).json({
+          error: true,
+          message: "User not found, please verify id"
+        });
+      }
+
+      return res.json({
+        error: false,
+        message: "User updated Okay"
+      });
+    } catch (error) {
+      return res.status(500).json({
+        error: true,
+        message: "Internal server error, please try later"
+      });
+    }
+  });
+
+  // Delete user by ID
+  app.delete('/users/delete/:id', async (req, res) => {
+    try {
+      const { id } = res.params;
+      await User.findOneAndDelete(id);
+
+      return res.json({
+        error: false,
+        message: "User deleted successfully !!!"
+      });
+    } catch (error) {
+      return res.status(500).json({
+        error: true,
+        message: "Internal server error, please try later"
+      });
+    }
+  });
 
   app.get('/', (req, res) => {
     res.status(201).send('Hello, World!');
